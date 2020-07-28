c2a01c0700f74e2e0364f5cd22b820d27175c723
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-28 17:06:13 +0200

    Create AnimatedSwitch component

---
+++ src/components/AnimatedSwitch.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import { CSSTransition, SwitchTransition, TransitionGroup } from 'react-transition-group';
+import { withRouter } from 'react-router';
+import uniqid from 'uniqid';
+
+const AnimatedSwitch = withRouter(({ location, children }) => {
+    
+    if ( typeof location.key === "undefined" ) {
+        // If the location contains no key, we manually 
+        // create one. This is necessary otherwise the 
+        // SwitchTransition will be stuck in an infinite
+        // loop between the -enter and -exit transitions.
+        location.key = uniqid();
+    }
+
+    return (
+        <SwitchTransition mode="out-in">
+            <CSSTransition 
+                key={location.key}
+                classNames="fade"
+                timeout={{enter: 250, exit: 250}}
+            >
+
+                <Switch location={location}>
+                    {children}
+                </Switch>
+
+            </CSSTransition>
+        </SwitchTransition>
+    )
+});
+
+export default AnimatedSwitch;
