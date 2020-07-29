5b7f9f7c08e7ea8e8da1e281783472b52490a2ff
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-29 12:56:15 +0200

    Change icon when opened state is chanegd

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,48 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition, Transition } from 'react-transition-group';
 
-const Nav = ( { toggleMenu, isOpen, isFixedNav, height } ) => {
-    const iconRef = useRef();
+const Nav = ({ toggleMenu, isOpen, isFixedNav, height }) => {
+    const [icon, setIcon] = useState(faBars);
+    const oldIsOpened = useRef(isOpen);
+    const iconTimeout = useRef(null);
+
+    useEffect(() => {
+        if (isOpen !== oldIsOpened.current) {
+            iconTimeout.current = setTimeout( () => {
+                setIcon( (isOpen) ? faTimes : faBars );
+            }, 100);
+            oldIsOpened.current = isOpen;
+        }
+    });
 
     return (
-        <> 
-            <nav style={ {height: height + "px"} }>
+        <>
+            <nav style={{ height: height + "px" }}>
                 <p>Maxime Esnol</p>
 
-                <div className="menu-icon" ref={iconRef.current}>
-                    <FontAwesomeIcon
-                        icon={(isOpen) ? faTimes : faBars}
-                        size={"lg"}
-                        onClick={toggleMenu} />
-                </div>
+
+                <Transition>
+                    <CSSTransition
+                        in={isOpen}
+                        classNames=""
+                        timeout={200}>
+                        <div className="menu-icon">
+                            <FontAwesomeIcon
+                                icon={icon}
+                                size={"lg"}
+                                onClick={toggleMenu} />
+
+                        </div>
+                    </CSSTransition>
+                </Transition>
             </nav>
-            
-            { isFixedNav && 
+
+            {
+                isFixedNav &&
                 <div className="nav-fix-height"
-                    style={ {height: height + "px"}}></div>
+                    style={{ height: height + "px" }}></div>
             }
         </>
     )
