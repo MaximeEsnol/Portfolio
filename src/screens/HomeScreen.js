eef372a8d33caeb467ff92824e9b7f2e67b89a4a
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-30 19:27:27 +0200

    Add proper content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import useTitle from '../hooks/useTitle';
+import { ScrollSnapContainer, SnapSection } from '../components/ScrollSnap';
 
 const HomeScreen = props => {
 
     useTitle("Maxime Esnol");
 
     return (
-        <div>
-            <h1>Maxime Esnol</h1>
-            <p>I create websites that are memorable, beautiful and easy to use.</p>
+        <div className="home">
+            <section>
+                <h1 className="multiline">
+                    <span className="line">
+                        Hi,
+                    </span>
+                    <span className="line">
+                        I'm Maxime
+                    </span>
+                </h1>
+
+                <p>
+                    I'm a passionate web developer who loves to 
+                    learn about new technologies, build memorable 
+                    user experiences and watch football.
+                </p>
+            </section>
         </div>
     )
 }
