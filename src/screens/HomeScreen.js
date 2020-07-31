d451caf6f226a9af0ea139dec9cc723ab00545ec
MaximeEsnol <maxime.esnol02@gmail.com>
2020-07-31 15:44:41 +0200

    Added a fab button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,41 +6,63 @@ const HomeScreen = props => {
 
     useTitle("Maxime Esnol");
 
-    const hiRef = useRef();
-    const nameRef = useRef();
-    const h1Ref = useRef();
-    const aboutRef = useRef();
-
     return (
         <div className="home">
             <section>
-                <h1 className="multiline" ref={h1Ref}>
+                <h1 className="multiline">
+                    {/** Delayed: 0 secs */}
                     <TimedTransition
                         started={true}
-                        duration={750}
+                        duration={2000}
                         classNames={""}>
-                        <span className="line home-hi" ref={hiRef}>
+                        <span className="line home-hi">
                             Hi,
                         </span>
                     </TimedTransition>
-
+                    {/** Starts at: 1200ms, ends at: 2200ms */}
                     <TimedTransition
                         started={true}
-                        duration={750}
-                        delay={450}
+                        duration={1000}
+                        delay={1200}
                         classNames={""}>
-                        <span className="line home-name" ref={nameRef}>
+                        <span className="line home-name">
                             I'm Maxime
                         </span>
                     </TimedTransition>
                     
                 </h1>
 
-                <p ref={aboutRef}>
-                    I'm a passionate web developer who loves to
-                    learn about new technologies, build memorable
-                    user experiences and watch football.
-                </p>
+                <div className="home-bio-container">
+                    {/** Starts at: 2800ms, ends at: 3800ms */}
+                    <TimedTransition
+                        started={true}
+                        duration={1000}
+                        delay={2800}
+                        classNames="">
+                        <p className="home-bio">
+                            I'm a passionate web developer who loves to
+                            learn about new technologies, build memorable
+                            user experiences and watch football.
+                        </p>
+                    </TimedTransition>
+                </div>
+
+                {/** Starts at: 4800ms, Ends at: 5800ms */}
+                <TimedTransition
+                    started={true}
+                    duration={1000}
+                    delay={4800}>
+                    <div className="fab-btn home-fab">
+                        <TimedTransition
+                            started={true}
+                            duration={500}
+                            delay={5300}>
+                            <i className="fas fa-chevron-down home-fab-icon"></i>
+                            </TimedTransition>
+                       
+                    </div>
+                </TimedTransition>
+                
             </section>
 
             <section>
