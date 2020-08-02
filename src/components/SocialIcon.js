22b3112dbde8bc6b908a62b3c6ee507f504cdb51
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-02 14:44:53 +0200

    Add option to also show username

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const SocialIcon = ( { social } ) => {
+const SocialIcon = ( { social, withUsername = false } ) => {
 
-    const { url, icon, accent } = social;
+    const { url, icon, accent, username } = social;
 
     return (
         <li>
             <a href={url} accentcolor={accent}>
                 <i className={icon}></i>
+                { withUsername && 
+                <span className="social__username">{username}</span>
+                }
             </a>
         </li>
     )
