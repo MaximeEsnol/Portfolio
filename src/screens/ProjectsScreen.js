c4f0b00a217adf450ecc8f15302e4d427894f019
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-03 16:05:56 +0200

    Load all projects

--- src/screens/ProjectsScreen.js
+++ src/screens/ProjectsScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import useTitle from '../hooks/useTitle';
+import ProjectPreview from '../components/ProjectPreview';
 
 const ProjectsScreen = props => {
 
     useTitle("My Work - Maxime Esnol");
 
+    const projects = useRef(require("./../assets/data/projects.json")).current;
+
     return(
         <div>
             <h1>My work</h1>
 
-            
+            {
+                projects.map( (project, index) => {
+                    return <ProjectPreview key={index} project={project}/>
+                })
+            }
         </div>
     )
 }
