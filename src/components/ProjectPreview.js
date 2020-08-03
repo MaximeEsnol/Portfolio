805959a9fda1326255d4e7bc0eec8be9ce104d0a
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-03 16:05:34 +0200

    Clicking on  project title opens project detaisl

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -1,51 +1,63 @@
 import React, { useState } from 'react';
 import TimedTransition from './../components/TimedTransition';
+import { Link } from 'react-router-dom';
 
 const ProjectPreview = ({ project, isAnimated = false }) => {
 
-    const [animatedClass, setAnimatedClass] = useState( (isAnimated) ? "animated" : "" );
+    const [animatedClass, setAnimatedClass] = useState((isAnimated) ? "animated" : "");
 
-    const title = (<h3>{project.name}</h3>);
+    const title = (
+        <h3>
+            <Link to={"/project/" + project.slug }>
+                {project.name}
+                <i className="fas fa-chevron-right"></i>
+            </Link>
+        </h3>
+    );
     const description = (<p>{project.description}</p>);
-    const image = (<img src={process.env.PUBLIC_URL + "/images/" + project.images[0].src} alt={project.images[0].alt} />);
+    const image = (
+        <div className="project-image"
+            style={{ backgroundImage: "url(" + process.env.PUBLIC_URL + "/images/" + project.images[0].src + ")"}}>
+        </div>
+    );
 
     return (
         <div className="project-container">
             <div className={"project " + animatedClass}>
                 <div className={"project-description"}>
                     {
-                        (isAnimated) ? 
-                        <TimedTransition
-                        started={true}
-                        delay={750}
-                        duration={3000}
-                        children={title}/>
-                        :
-                        title
+                        (isAnimated) ?
+                            <TimedTransition
+                                started={true}
+                                delay={750}
+                                duration={3000}
+                                children={title} />
+                            :
+                            title
                     }
 
-                    { 
+                    {
                         (isAnimated) ?
-                        <TimedTransition
-                        started={true}
-                        delay={2750}
-                        duration={1000}
-                        children={description}/>
-                        :
-                        description
+                            <TimedTransition
+                                started={true}
+                                delay={2750}
+                                duration={1000}
+                                children={description} />
+                            :
+                            description
                     }
-                    
+
                 </div>
 
                 {
                     (isAnimated) ?
-                    <TimedTransition
-                    started={true}
-                    delay={4250}
-                    duration={750}
-                    children={image}/>
-                    :
-                    image
+                        <TimedTransition
+                            started={true}
+                            delay={4250}
+                            duration={750}
+                            children={image} />
+                        :
+                        image
                 }
             </div>
         </div>
