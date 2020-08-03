c3b878f81182e53566e76b3bdfd33b1355916908
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-03 12:38:12 +0200

    Add Timeline to ExperienceScreen

--- src/screens/ExperienceScreen.js
+++ src/screens/ExperienceScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import useTitle from '../hooks/useTitle';
+import Timeline, { TimelineEntry } from './experiencescreen/Timeline';
+import TimedTransition from '../components/TimedTransition';
+import ReactVisibilitySensor from 'react-visibility-sensor';
 
 const ExperienceScreen = props => {
 
     useTitle("Experience - Maxime Esnol");
 
+    const entries = useRef(require("./../assets/data/experience.json")).current;
+
+    const entriesSorted = entries.sort( (a, b) => new Date(b.date) - new Date(a.date) );
+
     return(
-        <div>
-            <h1>My working experience</h1>
-            <p>Internships, studies. Find it here.</p>
+        <div className="experience">
+            <h1>My experience</h1>
+
+            <Timeline>
+            { entriesSorted.map( ( entry, index ) => {
+                let date = new Date(entry.date);
+                let year = date.getFullYear();
+                let month = date.toLocaleString('en', {month: 'short'});
+                let day = date.getDate();
+                return (
+                        <TimelineEntry key={index} 
+                        date={"" + day + " " + month + " " + year} 
+                        title={entry.title}>
+                            {entry.content}
+                        </TimelineEntry>
+                    )
+            }) }
+            </Timeline>
         </div>
     )
 }
