21b64213fa72f0fb0adb6a25eaf362917f5806f6
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-03 12:57:53 +0200

    Remove <p>

--- src/screens/ProjectsScreen.js
+++ src/screens/ProjectsScreen.js
@@ -8,7 +8,8 @@ const ProjectsScreen = props => {
     return(
         <div>
             <h1>My work</h1>
-            <p>Everything I've made. Websites, apps, programs.</p>
+
+            
         </div>
     )
 }
