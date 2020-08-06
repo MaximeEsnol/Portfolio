55609e47cf65f8e6b50bd37d30dd05b9ec16c2d0
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-06 14:43:29 +0200

    Previous/Next controller for sliders

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Slides = props => {
+const Slides = ( {children, ...props}) => {
+
+    const [ slideOffset, setSlideOffset ] = useState(0);
+    const slider = useRef();
+
+    useEffect( () => {
+        console.log(slider.current.offsetWidth);
+        slider.current.scrollTo({
+            top: 0,
+            left: (slider.current.offsetWidth * children.length) * slideOffset / 100,
+            behavior: "smooth"
+        }); 
+
+    } );
+    
+    const switchSlides = ( direction ) => {
+        let offsetPerSlide = 100 / children.length;
+
+        if ( direction === "previous" ) {
+            goToSlide(slideOffset - offsetPerSlide);
+        } else if ( direction === "next" ) {
+            goToSlide(slideOffset + offsetPerSlide);
+        }
+    }
+
+    const goToSlide = ( newOffset ) => {
+        if ( newOffset <= 0 ) {
+            setSlideOffset(0);
+            return;
+        }
+
+        if ( newOffset >= 100 ) {
+            setSlideOffset(100);
+            return;
+        }
+
+        setSlideOffset( newOffset );
+    }
 
     return(
-        <div {...props} className="slides">
-            { props.children }
+        <div className="slides-container">
+            <i className="fas fa-chevron-left" onClick={() => switchSlides.call(this, "previous")}></i>
+            <i className="fas fa-chevron-right" onClick={() => switchSlides.call(this, "next")}></i>
+            <div {...props} className="slides" ref={slider}>
+                { children }
+            </div>
         </div>
+        
     )
 }
 
