37295ca455a0f12b06a52688c7c5171632c5a5f7
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-06 19:01:50 +0200

    Updates

---
+++ data/project-data/box-shadow.md
@@ -0,0 +1,16 @@
+### Why?
+
+I've made the box shadow generator because I found myself looking way too often for a proper
+tool to generate beautiful CSS box shadow. All the tools that I could find, however, only allowed 
+one box shadow, even though the CSS specification allows for an element to have multiple shadows. 
+
+I then decided to make my own box shadow generator, with support for layered shadows. This 
+project has come very useful to myself already, and I am sure it could be of great use 
+for other web developers. 
+
+### How?
+
+Box Shadow Generator is built using React.js. This allows it to be fast and make all necessary 
+calculations in real time. When a new shadow is added or edited, the generated CSS is also 
+instantly updated. 
+For the page design, I haven't used any CSS frameworks. It was designed using pure CSS. 
---
+++ precache-manifest.6ed1b00bdcb5fd3292fcaff0dd59c75c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce841f3d5ce788a44229c08208fb0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fec68e791deb9297953",
+    "url": "/static/css/main.3cddd712.chunk.css"
+  },
+  {
+    "revision": "9364eb55d4151efef6c4",
+    "url": "/static/js/2.de22a054.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de22a054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fec68e791deb9297953",
+    "url": "/static/js/main.a87fa1b7.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fd5e5f32c1f438ecf5dd71d6ae01b049",
+    "url": "/static/media/Heebo-Bold.fd5e5f32.ttf"
+  },
+  {
+    "revision": "29bad8e0b0e6cf92f1f7d22f7fcfba79",
+    "url": "/static/media/Heebo-Light.29bad8e0.ttf"
+  },
+  {
+    "revision": "b4e0045784568bc212a3bb6de08891ff",
+    "url": "/static/media/Heebo-Medium.b4e00457.ttf"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/static/media/Heebo-Regular.3f5fef64.ttf"
+  },
+  {
+    "revision": "5f7303c0e7f09925586e218ab8fd9b19",
+    "url": "/static/media/PTSerif-Regular.5f7303c0.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
