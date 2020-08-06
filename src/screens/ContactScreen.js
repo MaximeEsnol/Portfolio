24b0d0d702ae15f129c45d84b7a85ffa586f5e48
MaximeEsnol <maxime.esnol02@gmail.com>
2020-08-06 18:55:07 +0200

    Contact form on contact screen

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,16 +1,16 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import useTitle from '../hooks/useTitle';
+import ContactForm from '../components/ContactForm';
 
 const ContactScreen = props => {
-    const hasMounted = useRef(false);
-
     useTitle("Contact me - Maxime Esnol");
 
     return( 
-        <div>
+        <section className={"contact"}>
             <h1>Get in touch</h1>
-            <p>Do you want more information? Want to talk? Have feedback? Let's talk, I am all ears.</p>
-        </div>
+            
+            <ContactForm title={"Mail me"}/>
+        </section>
     )
 }
 
